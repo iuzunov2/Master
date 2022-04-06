494d3b955b85a80ee3a9dc74ffa8ad75349a82fa
IckoUzunov <ickouzunov@yahoo.com>
2022-04-06 21:24:24 +0200

    commit2

--- Test/index.js
+++ Test/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+require('./pkg/db');
 
 const api = express()
 
 api.use(express.json())
+
+
+api.listen(config.get('service').port, err => {
+    if(err) return console.log(err);
+    console.log(`Server started on port ${config.get('service').port}`);
+});
